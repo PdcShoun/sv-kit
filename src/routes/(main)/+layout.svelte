7a2166f264e1edf68188c467f301d98a8672dafb
PdcShoun <pdc.pachara@gmail.com>
2024-08-21 19:14:16 +0700

    update version

--- src/routes/(main)/+layout.svelte
+++ src/routes/(main)/+layout.svelte
@@ -1,8 +1,14 @@
 <script>
 	import Nav from '$lib/layout/Nav.svelte'
+	import SideBar from '$lib/layout/SideBar.svelte'
 </script>
 
 <Nav />
-<div class="mx-auto max-w-screen-xl mt-8 border border-red-400">
-	<slot />
+<div class="container mx-auto flex justify-center gap-4 border border-red-400 md:justify-between">
+	<div class="hidden md:block">
+		<SideBar />
+	</div>
+	<div class="border border-green-400">
+		<slot />
+	</div>
 </div>
